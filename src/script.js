5d2808be5ea77f25afecfad4ed0ef8025f841504
cstephenson <chelseastephenson246@gmail.com>
2023-07-23 23:10:16 -0400

    use get recommendation endpoint to get better match based off listening history

--- src/script.js
+++ src/script.js
@@ -1,180 +1,120 @@
-// Because this is a literal single page application
-// we detect a callback from Spotify by checking for the hash fragment
-import { redirectToAuthCodeFlow, getAccessToken } from './authCodeWithPkce'
+import SpotifyApi from './spotifyApi'
 
-const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID
 const params = new URLSearchParams(window.location.search)
-const code = params.get('code')
 
-if (!code) {
-  redirectToAuthCodeFlow(clientId)
-  console.log(`i got here`)
-} else {
-  let accessToken = localStorage.getItem('spotify_access_token')
-  console.log(`this route ${accessToken}`)
+const SPOTIFY_CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID
+const SPOTIFY_AUTHORIZATION_CODE = params.get('code')
 
-  if (!accessToken) {
-    accessToken = await getAccessToken(clientId, code)
-  }
+let SPOTIFY_ACCESS_TOKEN
 
-  const topTracks = await getTopTracks(accessToken)
-  console.log(topTracks)
-  //console.log(topTracks.items[0].name)
-  let allTrackNames = Object.values(topTracks)[0]
-  console.log(allTrackNames)
-  let trackCleanedFields = allTrackNames.map(item => {
+const getUserDisplayName = async () => {
+    
+    const profile = await SpotifyApi.getUserProfile(SPOTIFY_ACCESS_TOKEN)
+    console.log(profile)
+    return profile.display_name
+}
+
+const getUsersTopTrackIds = async () => {
+  const topTracks = await SpotifyApi.getTopTracks(SPOTIFY_ACCESS_TOKEN, {
+    limit: 4
+  })
+  console.log(`toptracks`, topTracks)
+
+  return topTracks.items.map(item => item.id)
+}
+
+const getTopTaylorRecommendations = async topTrackIds => {
+  let recommendations = await SpotifyApi.getRecommendations(
+    SPOTIFY_ACCESS_TOKEN,
+    topTrackIds
+  )
+  console.log(`RECS:`)
+  console.log(recommendations)
+
+  recommendations = recommendations.tracks.map(item => {
     return {
-      name: item.name,
-      artist: item.artists[0].name,
-      link: item.href,
+      artistName: item.artists[0].name,
+      externalUrl: item.external_urls.spotify,
+      songName: item.name,
       id: item.id
     }
   })
-  let orderedTrackIds = trackCleanedFields.map(item => item.id)
-  console.log(orderedTrackIds)
 
-  console.log(trackCleanedFields)
+  return recommendations.filter(item => item.artistName === `Taylor Swift`)
+}
 
-  const trackInfo = await getTracks(accessToken, orderedTrackIds)
-  const allTrackInfo = Object.values(trackInfo)[0]
-  const allTrackInfoOrdered = allTrackInfo.map(item => {
-    return {
-      albumImage: item.album.images[0].url,
-      artistId: item.artists[0].id
-    }
-  })
-  const allArtistsIds = allTrackInfoOrdered.map(item => item.artistId)
-  const artistInfo = await getArtists(accessToken, allArtistsIds)
-  const allArtistInfo = Object.values(artistInfo)[0]
-  console.log(artistInfo)
-  const allArtistInfoOrdered = allArtistInfo.map(item => {
-    return {
-      artistName: item.name,
-      artistId: item.id,
-      genres: item.genres,
-      genreCount: item.genres.length
-    }
-  })
-  console.log(allArtistInfoOrdered)
-  let genreString = `rap`
-  const trackRecommendation = await getSearchResults(accessToken, genreString)
-  const allRecommendationInfo = Object.values(trackRecommendation)[0].items
-  console.log(allRecommendationInfo)
+const getFinalTaylorRecommendations = async topTrackIds => {
+  const uniqueRecommendationsById = {}
+  let requestCount = 0
 
-  const allRecommendationInfoOrdered = allRecommendationInfo.map(item => {
-    return {
-      albumName: item.album.name,
-      albumImage: item.album.images[0],
-      albumLink: item.href,
-      songName: item.name,
-      url: item.external_urls.spotify
-    }
-  })
+  while (
+    Object.keys(uniqueRecommendationsById).length < 6 &&
+    requestCount <= 5
+  ) {
+    let taylorRecommendations = await getTopTaylorRecommendations(topTrackIds)
 
-  const externalUrlsOrdered = allRecommendationInfoOrdered.map(item => {
-    return item.url
-  })
+    taylorRecommendations.forEach(item => {
+      uniqueRecommendationsById[item.id] = item
+    })
 
-  const hmtlList = []
-
-  for (const url of externalUrlsOrdered) {
-    const hmtlData = await getSpotifyEmbed(url)
-    const currentHTML = Object.values(hmtlData)[0]
-    hmtlList.push(currentHTML)
+    requestCount++
   }
 
-  // Cached UI Variables
-  const div1 = document.querySelector(`.div1`)
-  const div2 = document.querySelector(`.div2`)
-  const div3 = document.querySelector(`.div3`)
-  const div4 = document.querySelector(`.div4`)
-  const div5 = document.querySelector(`.div5`)
-  const div6 = document.querySelector(`.div6`)
+  console.log(`justTaylor`, uniqueRecommendationsById)
 
-  div1.innerHTML = hmtlList[0]
-  div2.innerHTML = hmtlList[1]
-  div3.innerHTML = hmtlList[3]
-  div4.innerHTML = hmtlList[4]
-  div5.innerHTML = hmtlList[5]
-  div6.innerHTML = hmtlList[6]
-
-  console.log(htmlList)
-  //const currentUrl = await getSpotifyEmbed(allRecommendationInfoOrdered[0].url)
-  //   const currentHTML = Object.values(currentUrl)[0]
-  //   console.log(currentHTML)
-
-  console.log(allRecommendationInfoOrdered)
-  //populateUI(profile)
+  return Object.values(uniqueRecommendationsById).map(item => item.externalUrl)
 }
 
-async function fetchProfile (code) {
-  const result = await fetch('https://api.spotify.com/v1/me', {
-    method: 'GET',
-    headers: { Authorization: `Bearer ${code}` }
+const updateRecommendationHtml = async (externalUrls) => {
+    const htmlEmbedPromises = externalUrls.map(async url => {
+        return await SpotifyApi.getEmbed(url)
+    })
+
+    const htmlEmbeds = (await Promise.all(htmlEmbedPromises)).map(item => item.html)
+  
+  const playerCardDivs = document.querySelectorAll(`div.main div`)
+  playerCardDivs.forEach((div, currentIndex) => {
+      div.innerHTML = htmlEmbeds[currentIndex]
   })
 
-  return await result.json()
 }
 
-async function getTopTracks (code) {
-  const result = await fetch('https://api.spotify.com/v1/me/top/tracks', {
-    method: 'GET',
-    headers: { Authorization: `Bearer ${code}` }
-  })
-
-  const topTracks = result.json()
-
-  return topTracks
-}
-
-async function getTracks (code, orderedTrackIds) {
-  let trackIdString = orderedTrackIds.toString()
-  console.log(`trackidstring: ${trackIdString}`)
-  const result = await fetch(
-    `https://api.spotify.com/v1/tracks?ids=${trackIdString}`,
-    {
-      method: 'GET',
-      headers: { Authorization: `Bearer ${code}` }
+const updateHeaderHtml = (displayName) => {
+    if(displayName){
+        const headerParagraph = document.querySelector(`h1`)
+        headerParagraph.innerHTML = `👋 Hello, ${displayName}.`
     }
-  )
-
-  const trackInfo = result.json()
-
-  return trackInfo
 }
 
-async function getArtists (code, allArtistsIds) {
-  const artistIdString = allArtistsIds.toString()
-  const result = await fetch(
-    `https://api.spotify.com/v1/artists?ids=${artistIdString}`,
-    {
-      method: 'GET',
-      headers: { Authorization: `Bearer ${code}` }
+const run = async () => {
+  if (!SPOTIFY_AUTHORIZATION_CODE) {
+    SpotifyApi.redirectToAuthCodeFlow(SPOTIFY_CLIENT_ID)
+    console.log(`i got here`)
+  } else {
+    SPOTIFY_ACCESS_TOKEN = localStorage.getItem('spotify_access_token')
+
+    if (!SPOTIFY_ACCESS_TOKEN) {
+      SPOTIFY_ACCESS_TOKEN = await SpotifyApi.getAccessToken(
+        SPOTIFY_CLIENT_ID,
+        SPOTIFY_AUTHORIZATION_CODE
+      )
     }
-  )
+  }
 
-  const artistInfo = result.json()
+  const displayName = await getUserDisplayName()
+  updateHeaderHtml(displayName)
+  let topTrackIds = await getUsersTopTrackIds()
+  let externalUrls =  await getFinalTaylorRecommendations(topTrackIds)
+  await updateRecommendationHtml(externalUrls)
 
-  return artistInfo
 }
 
-async function getSearchResults (code, genreString) {
-  const queryString = encodeURI(`artist:"taylor swift"&type=track`)
-  const result = await fetch(
-    `https://api.spotify.com/v1/search?q=${queryString}`,
-    {
-      method: 'GET',
-      headers: { Authorization: `Bearer ${code}` }
-    }
-  )
+//console.log(htmlList)
+//const currentUrl = await SpotifyApi.getEmbed(allRecommendationInfoOrdered[0].url)
+//   const currentHTML = Object.values(currentUrl)[0]
+//   console.log(currentHTML)
 
-  const trackRecommendation = result.json()
+//console.log(allRecommendationInfoOrdered)
 
-  return trackRecommendation
-}
+run()
 
-async function getSpotifyEmbed (url) {
-  const result = await fetch(`https://open.spotify.com/oembed?url=${url}`)
-
-  return result.json()
-}
